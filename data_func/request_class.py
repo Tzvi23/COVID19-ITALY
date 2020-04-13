127cc16c61e40c77d52d936f4985bbe4a7a4db58
tzvip <tzvi23@gmail.com>
2020-04-13 11:48:23 +0300

    Save & Load functions to pickle file formats

--- data_func/request_class.py
+++ data_func/request_class.py
@@ -3,6 +3,7 @@ import datetime
 import os
 import csv
 import pandas as pd
+import pickle
 
 
 def get_time():
@@ -10,14 +11,42 @@ def get_time():
     return date.strftime('%x').replace('/', '_')
 
 
+def load_obj(date, country=None):
+    """
+    Loads existing pickle file to obj.
+    :param date: search by date string format: 'DD_MM_YY'
+    :param country: If country supplied will look in the output folder for specific country dir
+    :return: load pickle file to variable
+    """
+    # Define paths
+    parent = os.path.sep.join(os.getcwd().split(os.path.sep)[:-1])  # Get parent folder
+    output_path = os.path.join(os.path.join(parent, 'output'))
+    history_path = os.path.join(output_path, 'all_history')
+    # Load data
+    if country is None:  # If country was not supplied means all history request
+        for files in os.listdir(history_path):
+            if files.replace('.pickle', '') == date:
+                with open(os.path.join(history_path, files), 'rb') as load_file:
+                    return pickle.load(load_file)
+    else:
+        for r, d, f in os.walk(output_path):
+            print(f'root: {f}, dir: {d}, file:{f}')
+            if (len(r) and country in r) and (len(f) and date + '.pickle' in f):
+                with open(os.path.join(r, date + '.pickle'), 'rb') as load_file:
+                    return pickle.load(load_file)
+    print(f'{date}.pickle not found!')
+
+
 class request:
     def __init__(self, name=get_time()):
         self.name = name
         self.base = 'https://corona.lmao.ninja/'  # API - endpoint
+        self.timePeriod = None
+        self.country = None
         self.URL = None
         self.response = None
         self.data = None
-        self.text = None
+        self.df = None
 
     def create_historical_country_request(self, countryName, daysBack=30):
         """
@@ -26,6 +55,8 @@ class request:
         """
         historical_country = f'v2/historical/{countryName}?lastdays={daysBack}'
         self.URL = self.base + historical_country
+        self.timePeriod = daysBack
+        self.country = countryName
         self.print_url()
 
     def create_historical_all_request(self, daysBack=30):
@@ -35,6 +66,7 @@ class request:
         """
         historical_all = f'v2/historical?lastdays={daysBack}'
         self.URL = self.base + historical_all
+        self.timePeriod = daysBack
         self.print_url()
 
     def send_request(self):
@@ -42,8 +74,8 @@ class request:
         Sends the request and stores the data in self.data as DICT
         """
         self.response = requests.get(url=self.URL)
-        self.text = self.response.text
         print(f'Request result: {self.response}')
+        self.df = pd.read_json(self.response.text)
         self.data = self.response.json()
 
     def print_url(self):
@@ -60,12 +92,12 @@ class request:
         country_dir = os.path.join(output_path_dir, self.data['country'])  # ./data_func/output/Israel
         if not os.path.exists(country_dir):  # check if country folder exists
             os.mkdir(country_dir)
-        timeline_dir = os.path.join(country_dir, 'timeline')  # ./data_func/output/Israel/timeline
-        if not os.path.exists(timeline_dir):
-            os.mkdir(timeline_dir)
+        # timeline_dir = os.path.join(country_dir, 'timeline')  # ./data_func/output/Israel/timeline
+        # if not os.path.exists(timeline_dir):
+        #     os.mkdir(timeline_dir)
 
         for sir in self.data['timeline'].items():
-            with open(os.path.join(timeline_dir, f'{sir[0]}_{self.name}.csv'), 'w') as output_file:
+            with open(os.path.join(country_dir, f'{sir[0]}_{self.name}.csv'), 'w') as output_file:
                 header = ['date', 'value']
                 dict_writer = csv.DictWriter(output_file, fieldnames=header)
 
@@ -73,10 +105,32 @@ class request:
                 for k, v in sir[1].items():
                     dict_writer.writerow({'date': k, 'value': v})
 
+    def save_obj(self):
+        """
+        Saves the created class to pickle file named as the class name.
+        The path is set to ./output/all_history as default.
+        If country specified in the class the path is set ot ./output/{country_name}
+        """
+        parent = os.path.sep.join(os.getcwd().split(os.path.sep)[:-1])  # Get parent folder
+        output_path_dir = os.path.join(os.path.join(parent, 'output'))  # ./data_func/output
+        if not os.path.exists(output_path_dir):  # check if output folder exists
+            os.mkdir(output_path_dir)  # If not create output folder
+        history_path = os.path.join(output_path_dir, 'all_history')  # ./data_func/output/all_history
+        if self.country is not None:
+            history_path = os.path.join(output_path_dir, self.country)
+        if not os.path.exists(history_path):
+            os.mkdir(history_path)
+        # Save pickle obj
+        with open(os.path.join(history_path, self.name + '.pickle'), 'wb') as save_file:
+            pickle.dump(self, save_file)
+            print(f'Saved class: {self.name}.pickle to: {history_path}')
+
 
 # Example
-x = request()
-x.create_historical_all_request(90)
-x.send_request()
-df = pd.read_json(x.text)
-print()
+# x = request()
+# x.create_historical_all_request(90)
+# x.send_request()
+# x.save_obj()
+
+x = load_obj('04_13_20', 'Israel')
+print(x)
